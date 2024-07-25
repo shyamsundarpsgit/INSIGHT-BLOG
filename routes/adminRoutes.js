41bebe2a5fe3af6c47d69c9df7e5c1d3ad1b24e3
shyamsundarpsgit <shyamsms466340@gmail.com>
2024-07-25 11:02:12 +0530

    page reverse fn added

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {viewAdminPage,adminLogIn, getAdminLogIn,getAdminSignUp, adminSignUp}= require('../controllers/adminController');
+const {viewAdminPage,adminLogIn, getAdminLogIn,getAdminSignUp, adminSignUp,getUserPostDetails,getAdminHome}= require('../controllers/adminController');
 
 
 router.get('/admin',viewAdminPage)
 router.get('/admin/login',getAdminLogIn);
 router.get('/admin/signup',getAdminSignUp);
 router.post('/admin/signup',adminSignUp);
+router.post('/admin/login',getUserPostDetails);
+router.get('/admin/home',getAdminHome);
 module.exports = router;
