a28fd41e6b706d178dde35c7e51668a99f97309a
shyamsundarpsgit <shyamsms466340@gmail.com>
2024-08-05 14:40:48 +0530

    Removed unwanted comments

--- middleware/sessionMiddleware.js
+++ middleware/sessionMiddleware.js
@@ -2,7 +2,7 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
 const sessionMiddleware = session({
-  secret: process.env.SESSION_SECRET, // Replace with your secret key
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({ mongoUrl: process.env.DB_URI }),
